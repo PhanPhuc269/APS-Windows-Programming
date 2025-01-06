7bd0b89959a85503038e573436d0d2e2160de31e
vow130504 <22120344@student.hcmus.edu.vn>
2025-01-06 12:57:19 +0700

    fix db

--- Database/seeds/002_account_seed.js
+++ Database/seeds/002_account_seed.js
@@ -8,7 +8,8 @@ exports.seed = function(knex) {
         { EMPLOYEE_ID: 3, EMP_NAME: 'User Two', EMP_ROLE: 'Staff', ACCESS_LEVEL: 2, USERNAME: 'user2', USER_PASSWORD: 'password', EMAIL: 'contact.quanminhle@gmail.com', SALARY: 3000000 },
         { EMPLOYEE_ID: 4, EMP_NAME: 'User Three', EMP_ROLE: 'Staff', ACCESS_LEVEL: 2, USERNAME: 'user3', USER_PASSWORD: 'password', EMAIL: 'leeqq3394@gmail.com', SALARY: 3000000  },
         { EMPLOYEE_ID: 5, EMP_NAME: 'User Four', EMP_ROLE: 'Staff', ACCESS_LEVEL: 2, USERNAME: 'user4', USER_PASSWORD: 'password', EMAIL: 'contact.quanminhle@gmail.com', SALARY: 3000000  },
-        { EMPLOYEE_ID: 6, EMP_NAME: 'Nguyen', EMP_ROLE: 'Admin', ACCESS_LEVEL: 1, USERNAME: 'Thienne', USER_PASSWORD: 'BwNrJBiApdz6LsE+Yp0sFd4KrKTLsgzvcixqSJ//4E8=:4ffca253-bcfd-4f6b-8a5c-fbefb58005be', SALARY: 3000000  }
+        { EMPLOYEE_ID: 6, EMP_NAME: 'Nguyen', EMP_ROLE: 'Admin', ACCESS_LEVEL: 1, USERNAME: 'Thienne', USER_PASSWORD: 'BwNrJBiApdz6LsE+Yp0sFd4KrKTLsgzvcixqSJ//4E8=:4ffca253-bcfd-4f6b-8a5c-fbefb58005be', SALARY: 3000000  },
+        { EMPLOYEE_ID: 7, EMP_NAME: 'NguyenMinh', EMP_ROLE: 'Staff', ACCESS_LEVEL: 2, USERNAME: 'Thienne2', USER_PASSWORD: 'BwNrJBiApdz6LsE+Yp0sFd4KrKTLsgzvcixqSJ//4E8=:4ffca253-bcfd-4f6b-8a5c-fbefb58005be', SALARY: 3000000  }
       ]);
     });
 };
