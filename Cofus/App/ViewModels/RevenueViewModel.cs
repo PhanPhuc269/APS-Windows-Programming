800a235df449f35bc76893282623a038ebbebf0c
QUAN LE MINH <22120290@student.hcmus.edu.vn>
2025-01-07 16:44:39 +0700

    thêm ngày

--- Cofus/App/ViewModels/RevenueViewModel.cs
+++ Cofus/App/ViewModels/RevenueViewModel.cs
@@ -1,4 +1,4 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -21,8 +21,8 @@ namespace App.ViewModels
         {
             get; set;
         }
-
         public int TransferAmount => TotalRevenue - CashAmount;
+
         public string TotalRevenueFormatted => $"{TotalRevenue:N0} VND";
         public string CashAmountFormatted => $"{CashAmount:N0} VND";
         public string TransferAmountFormatted => $"{TransferAmount:N0} VND";
@@ -31,31 +31,61 @@ namespace App.ViewModels
         public List<TopCategory> TopCategories { get; set; } = new();
         public List<TopSeller> TopSellers { get; set; } = new();
 
-        private DateTime _selectedDate;
-        public DateTime SelectedDate
+        // Thuộc tính ngày bắt đầu và ngày kết thúc
+        private DateTime _startDate;
+        public DateTime StartDate
         {
-            get => _selectedDate;
+            get => _startDate;
             set
             {
-                SetProperty(ref _selectedDate, value);
-                LoadRevenueData(value);
+                if (SetProperty(ref _startDate, value))
+                {
+                    OnPropertyChanged(nameof(StartDate)); // Đảm bảo kích hoạt
+                }
             }
         }
 
-        // Phương thức LoadRevenueData hiện tại
-        public async Task LoadRevenueData(DateTime selectedDate)
+        private DateTime _endDate;
+        public DateTime EndDate
         {
-            var revenueData = await new MySqlDao().GetRevenue(selectedDate);
-            var topProductsData = await new MySqlDao().GetTopProducts(selectedDate);
-            var topCategoriesData = await new MySqlDao().GetTopCategories(selectedDate);
-            var topSellersData = await new MySqlDao().GetTopSellers(selectedDate);
+            get => _endDate;
+            set
+            {
+                if (SetProperty(ref _endDate, value))
+                {
+                    OnPropertyChanged(nameof(EndDate)); // Đảm bảo kích hoạt
+                }
+            }
+        }
 
+
+        // Phương thức LoadRevenueData theo khoảng thời gian
+        public async Task LoadRevenueData(DateTime startDate, DateTime endDate)
+        {
+            var revenueData = await new MySqlDao().GetRevenue(startDate, endDate);
+            var topProductsData = await new MySqlDao().GetTopProducts(startDate, endDate);
+            var topCategoriesData = await new MySqlDao().GetTopCategories(startDate, endDate);
+            var topSellersData = await new MySqlDao().GetTopSellers(startDate, endDate);
+
+            // Cập nhật các thuộc tính với dữ liệu từ khoảng thời gian
             OrderCount = revenueData.OrderCount;
             TotalRevenue = revenueData.TotalRevenue;
             CashAmount = revenueData.CashAmount;
             TopProducts = topProductsData;
             TopCategories = topCategoriesData;
             TopSellers = topSellersData;
+
+            // Thông báo thay đổi dữ liệu
+            OnPropertyChanged(nameof(OrderCount));
+            OnPropertyChanged(nameof(TotalRevenue));
+            OnPropertyChanged(nameof(CashAmount));
+            OnPropertyChanged(nameof(TransferAmount));
+            OnPropertyChanged(nameof(TotalRevenueFormatted));
+            OnPropertyChanged(nameof(CashAmountFormatted));
+            OnPropertyChanged(nameof(TransferAmountFormatted));
+            OnPropertyChanged(nameof(TopProducts));
+            OnPropertyChanged(nameof(TopCategories));
+            OnPropertyChanged(nameof(TopSellers));
         }
     }
 }
